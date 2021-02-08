74e3aeea0eb89b00f167e50ffce08c4e35744205
hc <79045803@qq.com>
2021-02-08 11:14:32 +0800

    登陆改为模拟

--- src/services/auth.js
+++ src/services/auth.js
@@ -8,6 +8,14 @@ export function registerApi(user) {
     return post('/user/register', user)
 }
 
-export function loginApi(user) {
-    return post('/user/login', user)
+// export function loginApi(user) {
+//     return post('/user/login', user)
+// }
+
+// 模拟
+export async function loginApi(user) {
+    let res = {data:{}}
+    res.data.status = "success"
+    res.data.data = "token"
+    return res
 }
